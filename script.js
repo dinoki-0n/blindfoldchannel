b7445ac6b16b6111de77620f5109a2151fa4d615
Dinoki ON <franciscoruizgamboa4@gmail.com>
2025-03-28 16:35:44 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-
+// Xi*ro ponte fecha (como dicen los chavos)
     var fecha = new Date();
     var opciones = { year: 'numeric'};
     var fechaFormateada = fecha.toLocaleDateString('es-ES', opciones);
     document.getElementById('fechaActual').textContent = fechaFormateada;
+
+/*-- from the page #BYDINOKI */
